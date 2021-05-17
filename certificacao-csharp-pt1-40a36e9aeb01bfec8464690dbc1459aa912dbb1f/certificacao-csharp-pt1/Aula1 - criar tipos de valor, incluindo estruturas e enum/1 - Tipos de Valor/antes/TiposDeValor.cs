b04c9c1e2c3bc97a1bf1c0d4b183329e7a6328c2
lucasamacedo <lucas_am_@hotmail.com>
2021-05-17 15:41:15 -0300

    Conhecendo tipos de variáveis

---
+++ certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/1 - Tipos de Valor/antes/TiposDeValor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade;
+            idade = 30;
+            Console.WriteLine(idade);
+
+            int copiaIdade = idade;
+
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
+
+            idade = 23;
+
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
+
+            int? idade2 = null;
+            System.Nullable<int> idade3 = null;
+        }
+    }
+}
+//< package id = "Microsoft.Office.Interop.Excel" version = "15.0.4795.1000" targetFramework = "net461" />
+     
+    //   < package id = "Newtonsoft.Json" version = "13.0.1" targetFramework = "net461" />
