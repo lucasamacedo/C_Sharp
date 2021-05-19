413f0c49e62cd4ddb39f2c160bc911461e65804e
lucasamacedo <lucas_am_@hotmail.com>
2021-05-19 17:54:14 -0300

    Aprendendo sobre argumentos nomeados

--- certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/3 - Parametros Opcionais/antes/ParametrosNomeados.cs
+++ certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/3 - Parametros Opcionais/antes/ParametrosNomeados.cs
@@ -10,6 +10,21 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            //O método pode ser chamado do jeito normal, usando argumentos posicionais.
+            ImprimirDetalhesDoPedido("Maria de Fátima", 31, "Caneca Vermelha");
+
+            //Argumentos nomeados podem ser fornecidos para os parâmetros em qualquer ordem.
+            ImprimirDetalhesDoPedido(nomeProduto: "Caneca Vermelha", vendedor: "Maria de Fátima", numeroPedido: 31);
+            ImprimirDetalhesDoPedido(numeroPedido: 31, nomeProduto: "Caneca Vermelha", vendedor: "Maria de Fátima");
+
+            //Argumentos nomeados misturados com argumentos posicionais são válidos
+            // Argumentos posicionais devem ser passados como argumento
+            // na mesma posição que está como parâmetro no método correspondente
+            ImprimirDetalhesDoPedido("Maria de Fátima", 31, nomeProduto: "Caneca Vermelha");
+
+            // As 2 linhas abaixo geravam erro de compilação ANTES DO C# 7.2:
+            ImprimirDetalhesDoPedido(vendedor: "Maria de Fátima", 31, nomeProduto: "Caneca Vermelha");
+            ImprimirDetalhesDoPedido("Maria de Fátima", numeroPedido: 31, "Caneca Vermelha");
 
         }
 
