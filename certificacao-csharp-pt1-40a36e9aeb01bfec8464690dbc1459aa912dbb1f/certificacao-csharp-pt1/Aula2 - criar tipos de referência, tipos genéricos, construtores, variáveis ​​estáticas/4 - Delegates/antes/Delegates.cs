2f541aed4b031fdda87ae5b0b37938b896fa6980
lucasamacedo <lucas_am_@hotmail.com>
2021-05-19 03:06:42 -0300

    Aprendendo sobre Delegates/Delegados

--- certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/4 - Delegates/antes/Delegates.cs
+++ certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/4 - Delegates/antes/Delegates.cs
@@ -14,6 +14,8 @@ namespace certificacao_csharp_roteiro.antes
         }
     }
 
+    delegate double MetodoMultiplicacao(double input);
+
     class Calculadora
     {
         static double Duplicar(double input)
@@ -34,6 +36,27 @@ namespace certificacao_csharp_roteiro.antes
 
             //Executa diretamente o método
             Console.WriteLine($"Triplicar(7.5): {Triplicar(7.5)}");
+
+            MetodoMultiplicacao metodoMultiplicacao = Duplicar;
+            Console.WriteLine(metodoMultiplicacao(7.5));
+
+            metodoMultiplicacao = Triplicar;
+            Console.WriteLine(metodoMultiplicacao(7.5));
+
+            // instancia um delegado com um método anônimo
+            MetodoMultiplicacao metodoQuadrado = delegate (double input)
+            {
+                return input * input;
+            };
+
+            double quadrado = metodoQuadrado(5);
+            Console.WriteLine("quadrado: {0}", quadrado);
+
+            // instancia um delegado com uma expressão lambda
+            MetodoMultiplicacao metodoCubo = input => input * input * input;
+
+            double cubo = metodoCubo(4.375);
+            Console.WriteLine("cubo: {0}", cubo);
         }
     }
 }
