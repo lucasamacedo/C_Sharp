8f34f5a291031c5e7b5cf294451157d1a6e1bda4
lucasamacedo <lucas_am_@hotmail.com>
2021-05-19 02:24:51 -0300

    Aprendendo sobre classes

--- certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/2 - Classes/antes/Classes.cs
+++ certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/2 - Classes/antes/Classes.cs
@@ -10,6 +10,63 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            ClassePosicaoGPS posicao1 = new ClassePosicaoGPS()
+                { Latitude = 13.78, Longitude = 29.51 };
+            posicao1.Latitude = 13.78;
+            posicao1.Longitude = 29.51;
+            //posicao1 = new ClassePosicaoGPS(13.78, 29.51);
+            Console.WriteLine(posicao1);
+
+            PosicaoGPSComLeitura posicao2 = new PosicaoGPSComLeitura(13.78, 29.51, DateTime.Now);
+            Console.WriteLine(posicao2);
+        }
+
+       
+    }
+    interface IGPS
+    {
+        bool EstaNoHemisferioNorte();
+    }
+
+    public class ClassePosicaoGPS : IGPS
+    {
+        public double Latitude;
+        public double Longitude;
+
+        public ClassePosicaoGPS()
+        {
+        }
+
+        public ClassePosicaoGPS(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public bool EstaNoHemisferioNorte()
+        {
+            return Latitude > 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Latitude: {Latitude}, Longitude: {Longitude}";
+        }
+        //trecho do código omitido
+    }
+
+    public class PosicaoGPSComLeitura : ClassePosicaoGPS
+    {
+        private readonly DateTime dataLeitura;
+
+        public PosicaoGPSComLeitura(double latitude, double longitude, DateTime dataLeitura) : base(latitude, longitude)
+        {
+            this.dataLeitura = dataLeitura;
+        }
+
+        public override string ToString()
+        {
+            return $"Latitude: {Latitude}, Longitude: {Longitude}, Data Leitura: {dataLeitura}";
         }
     }
 }
