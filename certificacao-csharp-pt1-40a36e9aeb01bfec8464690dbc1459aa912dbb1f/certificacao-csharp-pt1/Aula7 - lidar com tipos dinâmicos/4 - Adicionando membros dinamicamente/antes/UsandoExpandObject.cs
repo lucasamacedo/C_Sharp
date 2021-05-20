a839c500dcecd5adc4f06ac22ff95a86ec1f58e7
lucasamacedo <lucas_am_@hotmail.com>
2021-05-20 04:00:15 -0300

    Utilizand a classe ExpandoObject

--- certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula7 - lidar com tipos dinâmicos/4 - Adicionando membros dinamicamente/antes/UsandoExpandObject.cs
+++ certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula7 - lidar com tipos dinâmicos/4 - Adicionando membros dinamicamente/antes/UsandoExpandObject.cs
@@ -14,6 +14,23 @@ namespace certificacao_csharp_roteiro.antes
         {
             string json = "{\"De\": \"Paulo Silveira\"," +
                 "\"Para\": \"Guilherme Silveira\"}";
+
+            dynamic mensagem = JsonConvert.DeserializeObject<ExpandoObject>(json);
+
+            mensagem.Texto = "Olá, " + mensagem.Para;
+
+            EnviarMensagem(mensagem);
+
+            mensagem.Inverter = new Action(() =>
+            {
+                var aux = mensagem.De;
+                mensagem.De = mensagem.Para;
+                mensagem.Para = aux;
+                mensagem.Texto = "Olá, " + mensagem.Para;
+            });
+
+            mensagem.Inverter();
+            EnviarMensagem(mensagem);
         }
 
         private void EnviarMensagem(dynamic msg)
@@ -24,4 +41,12 @@ namespace certificacao_csharp_roteiro.antes
             Console.WriteLine();
         }
     }
+
+    //class Mensagem
+    //{
+    //    public string De { get; set; }
+    //    public string Para { get; set; }
+    //    public string Texto { get; set; }
+
+    //}
 }
