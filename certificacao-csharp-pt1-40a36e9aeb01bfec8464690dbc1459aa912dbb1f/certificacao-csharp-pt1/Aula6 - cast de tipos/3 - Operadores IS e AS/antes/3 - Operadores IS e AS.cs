24c7dabbc3bd6894893781f92f408105170efab4
lucasamacedo <lucas_am_@hotmail.com>
2021-05-20 00:45:28 -0300

    Aprendendo sobre operadores IS e AS

--- certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula6 - cast de tipos/3 - Operadores IS e AS/antes/3 - Operadores IS e AS.cs
+++ certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula6 - cast de tipos/3 - Operadores IS e AS/antes/3 - Operadores IS e AS.cs
@@ -12,14 +12,31 @@ namespace certificacao_csharp_roteiro.antes
         {
             Animal animal = new Animal();
             Gato gato = new Gato();
-            //Cliente cliente = new Cliente("José da Silva", 30);
+            Cliente cliente = new Cliente("José da Silva", 30);
+
+            Alimentar(animal);
+            Alimentar(gato);
+            Alimentar(cliente);
         }
 
         public void Alimentar(object obj)
         {
-            Animal animal = (Animal)obj;
-            animal.Beber();
-            animal.Comer();
+            // operador is ve se uma variavel é de determinado tipo
+            // operador as faz o cast explícito com segurança
+
+            //Animal animal = obj as Animal;
+
+            //if (obj is IAnimal)
+            //{
+            //    animal = (Animal)obj;
+            //}
+            if (obj is Animal animal) // se der certo, declara uma variavel chamada animal e vai receber o cast caso essa conversão possa ser feita
+            {
+                animal.Beber();
+                animal.Comer();
+                return;
+            }
+            Console.WriteLine("obj não é um animal");
         }
     }
 }
