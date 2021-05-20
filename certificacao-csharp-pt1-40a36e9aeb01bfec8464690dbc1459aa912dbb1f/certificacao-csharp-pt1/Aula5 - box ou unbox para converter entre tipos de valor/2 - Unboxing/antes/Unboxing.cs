2f15d1f67e739efdc1c08e1ddccd39af07d4d434
lucasamacedo <lucas_am_@hotmail.com>
2021-05-20 00:01:42 -0300

    Aprendendo sobre boxing e unboxing

---
+++ certificacao-csharp-pt1-40a36e9aeb01bfec8464690dbc1459aa912dbb1f/certificacao-csharp-pt1/Aula5 - box ou unbox para converter entre tipos de valor/2 - Unboxing/antes/Unboxing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro.antes
+{
+    class Unboxing : IAulaItem
+    {
+        public void Executar()
+        {
+            int numero = 57;      // tipo de valor
+            object caixa = numero;
+
+            try
+            {
+                int unboxed = (int) caixa;
+                Console.WriteLine("Unboxing OK.");
+            }
+            catch (System.InvalidCastException e)
+            {
+                Console.WriteLine("{0} Erro: unboxing incorreto. ", e);
+            }
+        }
+    }
+}
