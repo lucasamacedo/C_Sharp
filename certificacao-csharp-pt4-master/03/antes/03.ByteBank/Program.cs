3f792dd063e3f4caf7bd4ba778daa1785b22f9b8
lucasamacedo <lucas_am_@hotmail.com>
2021-05-21 03:40:08 -0300

    Lendo linhas de um arquivo utilizando estrutura while

--- certificacao-csharp-pt4-master/03/antes/03.ByteBank/Program.cs
+++ certificacao-csharp-pt4-master/03/antes/03.ByteBank/Program.cs
@@ -7,6 +7,14 @@ namespace _03.ByteBank
     {
         static void Main(string[] args)
         {
+            GetFatorial(5);
+            GetFatorial(4);
+            GetFatorial(3);
+            GetFatorial(2);
+            GetFatorial(1);
+            GetFatorial(0);
+
+            RelatorioClientes.ImprimirListagemClientes();
         }
 
         private static int GetFatorial(int numero)
@@ -19,6 +27,13 @@ namespace _03.ByteBank
             //FATORIAL DE 0                      = 1 
 
             int fatorial = 1;
+            int fator = numero;
+
+            while(fator > 0)
+            {
+                fatorial *= fator;
+                fator--;
+            }
 
             System.Console.WriteLine($"fatorial de {numero} é {fatorial}");
 
