04017a506626ed1d73cb562edb645336fd2e4b22
lucasamacedo <lucas_am_@hotmail.com>
2021-05-27 23:34:36 -0300

    Aprendendo a usar dicionarios

---
+++ certificacao-csharp-pt6-master/Exercicio2HashSet/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exercicio2HashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpColecoes = new Curso("C# Colecoes", "Marcelo Oliveira");
+
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            csharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
+            csharpColecoes.Adiciona(new Aula("Modelando com Coleções", 24));
+
+            Aluno a1 = new Aluno("Vanessa Tonini", 34672);
+            Aluno a2 = new Aluno("Ana Losnak", 5617);
+            Aluno a3 = new Aluno("Rafael Nercessian", 17645);
+
+            csharpColecoes.Matricula(a1);
+            csharpColecoes.Matricula(a2);
+            csharpColecoes.Matricula(a3);
+
+
+        }
+    }
+
+    class Aluno
+    {
+        private string nome;
+        private int numeroMatricula;
+
+        public Aluno(string nome, int numeroMatricula)
+        {
+            this.nome = nome;
+            this.numeroMatricula = numeroMatricula;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int NumeroMatricula
+        {
+            get { return numeroMatricula; }
+            set { numeroMatricula = value; }
+        }
+    }
+}
---
+++ certificacao-csharp-pt6-master/ExercicioHashSet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISet<string> alunos = new HashSet<string>();
+
+            alunos.Add("Vanessa Tonini");
+            alunos.Add("Ana Losnak");
+            alunos.Add("Rafael Nercessian");
+
+            Console.WriteLine(string.Join(", ", alunos));
+
+            alunos.Add("Priscila Stuani");
+            alunos.Add("Rafael Rollo");
+            alunos.Add("Fabio Gushiken");
+
+            Console.WriteLine(string.Join(", ", alunos));
+
+            alunos.Remove("Ana Losnak");
+            alunos.Add("Marcelo Oliveira");
+
+            Console.WriteLine(string.Join(",", alunos));
+
+            alunos.Add("Fabio Gushiken");
+
+            //alunos.Sort();
+
+            List<string> alunosEmLista = new List<string>(alunos);
+            alunosEmLista.Sort();
+
+            Console.WriteLine(string.Join(", ", alunosEmLista));
+        }
+    }
+}
