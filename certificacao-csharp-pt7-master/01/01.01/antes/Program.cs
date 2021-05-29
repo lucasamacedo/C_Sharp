be8a4b0b5560b7a1cc76d2ce53394fc2e2b6adcf
lucasamacedo <lucas_am_@hotmail.com>
2021-05-28 21:32:06 -0300

    Associando e desassociando metodos de delegates

--- certificacao-csharp-pt7-master/01/01.01/antes/Program.cs
+++ certificacao-csharp-pt7-master/01/01.01/antes/Program.cs
@@ -7,14 +7,31 @@ namespace _01_01
         static void Main(string[] args)
         {
             Campainha campainha = new Campainha();
+            campainha.OnCampainhaTocou += CampainhaTocou1;
+            campainha.OnCampainhaTocou += CampainhaTocou2;
+            Console.WriteLine("A campainha será tocada");
             campainha.Tocar();
 
+            campainha.OnCampainhaTocou -= CampainhaTocou2;
+            Console.WriteLine("A campainha será tocada");
+            campainha.Tocar();
+
+        }
+
+        static void CampainhaTocou1()
+        {
+            Console.WriteLine("A campainha tocou. (1)");
+        }
+        static void CampainhaTocou2()
+        {
+            Console.WriteLine("A campainha tocou. (2)");
         }
     }
 
     class Campainha
     {
         public Action OnCampainhaTocou { get; set; }
+        //public Acao OnCampainhaTocou { get; set; }
         public void Tocar()
         {
             if (OnCampainhaTocou != null)
@@ -22,5 +39,6 @@ namespace _01_01
                 OnCampainhaTocou();
             }
         }
+        //public delegate void Acao();
     }
 }
